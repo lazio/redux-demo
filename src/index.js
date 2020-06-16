8a203aa6d9c3989128eb0e48819a11828985b544
lazio <diniovskiy@gmail.com>
2020-06-16 17:46:44 +0300

    redux inspector

--- src/index.js
+++ src/index.js
@@ -8,7 +8,10 @@ import * as serviceWorker from "./serviceWorker";
 
 import reducers from "./reducers";
 
-const store = createStore(reducers);
+const store = createStore(
+  reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
   <React.StrictMode>
